33ea32982df354d495330519a3848e23db4f2b2b
ManticoreDai <a136724262@gmail.com>
2023-03-21 20:14:04 +0800

    add attack pixel range limit

--- utils/attacker.py
+++ utils/attacker.py
@@ -6,7 +6,7 @@ import func_timeout
 from utils.recorder import RandomAttackRecorder
 
 class RandomImageAttacker:
-    def __init__(self, model_path, n_img, attack_img_base, original_img, attack_pixels, total_timeout):
+    def __init__(self, model_path, n_img, attack_img_base, original_img, attack_pixels, total_timeout, limit=None):
         from tensorflow import keras
 
         self.n_img = n_img # 代表每個 iteration 要產生幾張隨機攻擊的圖
@@ -15,6 +15,7 @@ class RandomImageAttacker:
         self.original_img = original_img
         self.attack_pixels = attack_pixels
         self.total_timeout = total_timeout
+        self.limit = limit # example 0.1, [x*0.9, x*1.1]
 
         self.recorder: RandomAttackRecorder = None
         self.iter = 0
@@ -22,8 +23,12 @@ class RandomImageAttacker:
     
     def attack_once(self):
         self.recorder.iter_start()
-        attack_imgs = self._get_all_0_255_img_multi_pixels()
         
+        if self.limit is None:
+            attack_imgs = self._get_all_0_255_img_multi_pixels()
+        else:
+            attack_imgs = self._get_all_limit_img_multi_pixels()
+                    
         original_atk_imgs = np.append(self.original_img, attack_imgs, axis=0)
         all_attack_result = self.model.predict(original_atk_imgs).argmax(axis=1)
         pred_label = all_attack_result[0]
@@ -66,7 +71,6 @@ class RandomImageAttacker:
         self.recorder.end()
 
     
-
     def _get_all_0_255_img_multi_pixels(self):
         n_pixel = len(self.attack_pixels)
 
@@ -77,6 +81,28 @@ class RandomImageAttacker:
         for img, rand_values in zip(attack_imgs, attack_comb):
             for attack_pixel, rand_val in zip(self.attack_pixels, rand_values):
                 row, col = attack_pixel
+                img[row, col, 0] = rand_val
+
+        attack_imgs = attack_imgs.astype("float32") / 255
+
+        # 回傳 攻擊過的圖片 n_img 張
+        return attack_imgs
+    
+    
+    def _get_all_limit_img_multi_pixels(self):
+        n_pixel = len(self.attack_pixels)
+
+        # 隨機改動多個pixel，產生 n_img 張攻擊的圖 limit
+        attack_imgs = np.repeat(np.expand_dims(self.attack_img_base, 0), self.n_img, axis=0)
+        
+        for img in attack_imgs:
+            for attack_pixel in self.attack_pixels:
+                row, col = attack_pixel
+                cur_value = img[row, col, 0]
+                lb = max(0, int(cur_value * (1-self.limit)))
+                ub = min(255, int(cur_value * (1+self.limit)))
+                rand_val = np.random.randint(lb, ub)
+                
                 img[row, col, 0] = rand_val
 
         attack_imgs = attack_imgs.astype("float32") / 255
